5523a0fd0dd2a656bfabf6062d44fef86d9f3b95
Jose Daniel Maury <jdmaury94@gmail.com>
2019-10-01 17:18:54 -0500

    Promises, async/await, waiting for multiple promises

--- 3-asynchronous-JS/starter/index.js
+++ 3-asynchronous-JS/starter/index.js
@@ -24,18 +24,51 @@ const getDocPic = async () => {
     const data = await readFilePro(`${__dirname}/dog.txt`);
     console.log(`Breed ${data}`);
 
-    const res = await superagent.get(
-      `https://dog.ceo/a3pi/breed/${data}/images/random`
+    const res1Pro = await superagent.get(
+      `https://dog.ceo/api/breed/${data}/images/random`
     );
-    console.log(res.body.message);
 
-    await writeFilePro('dog-img.txt', res.body.message);
+    const res2Pro = await superagent.get(
+      `https://dog.ceo/api/breed/${data}/images/random`
+    );
+
+    const res3Pro = await superagent.get(
+      `https://dog.ceo/api/breed/${data}/images/random`
+    );
+
+    const all = await Promise.all([res1Pro, res2Pro, res3Pro]);
+    const imgs = all.map(el => el.body.message);
+    console.log(imgs);
+
+    await writeFilePro('dog-img.txt', imgs.join('\n'));
     console.log('Random dog image saved to file!');
   } catch (e) {
     console.log(e);
+    throw err;
   }
+  return '2: READY 🐶';
 };
-getDocPic();
+//---------SOLUTION 1
+(async () => {
+  try {
+    console.log('1: Will get doc pics!');
+    const x = await getDocPic();
+    console.log(x);
+    console.log('1: Will get doc pics!');
+  } catch (error) {
+    console.log(err);
+  }
+})();
+//---------SOLUTION 2
+/* console.log('1: Will get doc pics!');
+getDocPic()
+  .then(x => {
+    console.log(x);
+     console.log('1: Will get doc pics!');
+  })
+  .catch(err => {
+    console.log('ERROR!!!!!');
+  }); */
 
 /* readFilePro(`${__dirname}/dog.txt`)
   .then(data => {
