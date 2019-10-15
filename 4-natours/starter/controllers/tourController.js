88c908e455ac2ed3ec9bc2d3b89cd1e42a8fe69f
Jose Daniel Maury <jdmaury94@gmail.com>
2019-10-15 17:00:26 -0500

    Filtering query using req.query

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -2,7 +2,21 @@ const Tour = require('../models/tourModel');
 
 exports.getAlltours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    //BUILD QUERY
+    const queryObj = { ...req.query };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach(el => delete queryObj[el]);
+
+    const query = Tour.find(queryObj);
+    const tours = await query;
+
+    /* 
+    const tours = await Tour.find()
+      .where('duration')
+      .equals(5)
+      .where('difficulty')
+      .equals('easy'); */
+
     res.status(200).json({
       status: 'success',
       results: tours.length,
