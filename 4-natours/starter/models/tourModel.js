6d88a2deb1e635c40ce381d4c5931d579669bc5a
Jose Daniel Maury <jdmaury94@gmail.com>
2019-10-24 17:45:31 -0500

    Document, Query and Aggregation middleware, Built-in validators

--- 4-natours/starter/models/tourModel.js
+++ 4-natours/starter/models/tourModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -6,8 +7,12 @@ const tourSchema = new mongoose.Schema(
       type: String,
       required: [true, 'A tour must have a name'],
       unique: true,
-      trim: true
+      trim: true,
+      maxlength: [40, 'A tour name must have less or equal than 40 characters'],
+      minlength: [10, 'A tour name must have more or equal than 10 characters']
     },
+    slug: String,
+
     duration: {
       type: Number,
       required: [true, 'A tour must have a duration']
@@ -18,12 +23,18 @@ const tourSchema = new mongoose.Schema(
     },
     difficulty: {
       type: String,
-      required: [true, 'A tour must have a difficulty']
+      required: [true, 'A tour must have a difficulty'],
+      enum: {
+        values: ['easy', 'medium', 'difficult'],
+        message: 'Difficulty is either: easy, medium, difficult'
+      }
     },
 
     ratingsAverage: {
       type: Number,
-      default: 4.5
+      default: 4.5,
+      min: [1, 'Rating must be above 1.0'],
+      max: [5, 'Rating must be below 5.0']
     },
     ratingsQuantity: {
       type: Number,
@@ -53,7 +64,11 @@ const tourSchema = new mongoose.Schema(
       default: Date.now(),
       select: false
     },
-    startDates: [Date]
+    startDates: [Date],
+    secretTour: {
+      type: Boolean,
+      default: false
+    }
   },
   {
     toJSON: { virtuals: true },
@@ -64,6 +79,45 @@ const tourSchema = new mongoose.Schema(
 tourSchema.virtual('durationWeeks').get(function() {
   //Available as soon as we get data
   return this.duration / 7;
+});
+
+//DOCUMENT MIDDLEWARE: runs before .save() and .create()
+//tourSchema.pre('hook',callback())
+tourSchema.pre('save', function(next) {
+  //console.log(this);
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+/* tourSchema.pre('save', function(next) {
+  console.log('Will save document...');
+  next();
+});
+
+tourSchema.post('save', function(doc, next) {
+  console.log(doc);
+  next();
+}); */
+
+//QUERY MIDDLEWARE
+tourSchema.pre(/^find/, function(next) {
+  //This points to current query
+  this.find({ secretTour: { $ne: true } });
+  this.start = Date.now();
+  next();
+});
+
+tourSchema.post(/^find/, function(docs, next) {
+  //After the query has already executed
+  console.log(`Query took ${Date.now() - this.start} milliseconds`);
+  //console.log(docs);
+  next();
+});
+
+//AGGREGATION MIDDLEWARE
+tourSchema.pre('aggregate', function(next) {
+  this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
+  next();
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
