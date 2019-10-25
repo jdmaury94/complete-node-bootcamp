0d75188e0d649d713b3a3a745f861bccfba49f1b
Jose Daniel Maury <jdmaury94@gmail.com>
2019-10-25 18:39:05 -0500

    Implementing a Global Error Handling Middleware

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -24,11 +24,24 @@ app.use('/api/v1/tours', tourRouter); //Apply tourRouter middleware to /api/v1/t
 app.use('/api/v1/users', userRouter); //Apply userRouter middleware to /api/v1/users route
 
 app.all('*', (req, res, next) => {
-  res.status(404).json({
+  /*   res.status(404).json({
     status: 'fail',
     message: `Can't find ${req.originalUrl} on this server!`
-  });
+  }); */
+  const err = new Error(`Can't find ${req.originalUrl} on this server!`);
+  err.status = 'fail';
+  err.statusCode = 404;
+  next(err);
 }); //It will run for all the verbs, HTTP methods
+
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message
+  });
+});
 
 //4) START SERVER
 
