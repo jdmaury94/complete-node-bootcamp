71c9beeb573cffaa8316523c85ee9c2b4e04ca5b
Jose Daniel Maury <jdmaury94@gmail.com>
2019-11-08 17:24:59 -0500

    Implementing rate limiting, security http headers, data sanitization and parameter pollution

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
+
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
@@ -7,14 +13,51 @@ const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
-//1) MIDDLEWARES
+//1) GLOBAL MIDDLEWARES
+//Set security HTTP headers
+app.use(helmet());
+
+//Development logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+//Limit requests from same API
+const limiter = rateLimit({
+  //100 requests from the same ip in 1 hour
+  max: 122,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!'
+});
+app.use('/api', limiter); //It will affect all the routes
 
-app.use(express.json());
+//Body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+
+//Data sanitization against NoSQL query injection
+app.use(mongoSanitize()); //Take a look at the whole req.body, req.query, req.params
+//filter out dots and dollar signs
+
+//Data sanitization against XSS(cross-site scripting attacks)
+app.use(xss()); //Clean the user input from malicious HTML code with js included
+
+//Prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price'
+    ]
+  })
+);
+
+//Serving static files
 app.use(express.static(`${__dirname}/public`));
 
+//Test middleware
 app.use((req, res, next) => {
   req.requestTIme = new Date().toISOString(); //Each time
   //  console.log(req.headers);
