d2fa94f80eecdae479897b26f977fd7312519189
Jose Daniel Maury <josedanielmaury@ZCOBMTH100250-JoseDanielMaury.local>
2020-03-10 17:23:12 -0500

    196. Updating User Password with Our API

--- 4-natours/starter/public/js/index.js
+++ 4-natours/starter/public/js/index.js
@@ -1,11 +1,15 @@
 import '@babel/polyfill'
 import {displayMap} from './mapbox'
 import {login,logout} from './login'
+import {updateSettings} from './updateSettings'
 
 //DOM ELEMENTS
 const mapBox = document.getElementById('map');
 const loginForm = document.querySelector('.form--login');
 const logoutBtn = document.querySelector('.nav__el--logout');
+const userDataForm = document.querySelector('.form-user-data');
+const userPasswordForm = document.querySelector('.form-user-password');
+
 
 //DELEGATION
 if(mapBox){
@@ -23,3 +27,27 @@ if(loginForm){
 }
 
 if(logoutBtn) logoutBtn.addEventListener('click',logout);
+
+if(userDataForm) userDataForm.addEventListener('submit',e=>{
+    e.preventDefault();
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    updateSettings({name,email},'data')
+
+})
+
+if(userPasswordForm) 
+userPasswordForm.addEventListener('submit',async e=>{
+    e.preventDefault();
+    document.querySelector('.btn--save-password').innerHTML = 'Updating...'
+    const passwordCurrent = document.getElementById('password-current').value;
+    const password = document.getElementById('password').value;
+    const passwordConfirm = document.getElementById('password-confirm').value;
+    await updateSettings({passwordCurrent,password,passwordConfirm},'password');
+    document.querySelector('.btn--save-password').innerHTML = 'Save Password';
+
+    document.getElementById('password-current').value = '';
+    document.getElementById('password').value = '';
+    document.getElementById('password-confirm').value = '';
+
+})
