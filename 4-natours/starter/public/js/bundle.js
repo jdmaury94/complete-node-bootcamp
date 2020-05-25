e22a57b9ccb7d5a4a71f09ffa3af359945822194
Jose Daniel Maury <josedanielmaury@ZCOBMTH100250-JoseDanielMaury.local>
2020-05-25 12:16:15 -0500

    207. Sending Password Reset Emails

--- 4-natours/starter/public/js/bundle.js
+++ 4-natours/starter/public/js/bundle.js
@@ -8988,7 +8988,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50563" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54581" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
